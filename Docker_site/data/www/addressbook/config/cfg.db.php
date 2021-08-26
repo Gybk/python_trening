427c9bf078efce8d7a5d1b2b20b2a01d18b80d4a
Roman <Romanchybarov@gmail.com>
2021-08-26 15:34:34 +0300

    Fix for README file

---
+++ Docker_site/data/www/addressbook/config/cfg.db.php
@@ -0,0 +1,8 @@
+<?php
+$dbserver     = "addressbook_db";
+$dbname       = "addressbook";
+$dbuser       = "addressbook";
+$dbpass       = "addressbook";
+$table_prefix = "";
+$keep_history = true;
+?>
